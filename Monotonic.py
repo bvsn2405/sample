1347a8b50c794ea7eca0395815a0ce328a43e91a
bvsn2405 <127843818+bvsn2405@users.noreply.github.com>
2023-04-18 08:49:01 +0530

    Update Monotonic.py

--- Monotonic.py
+++ Monotonic.py
@@ -17,3 +17,23 @@ A = [6, 5, 4, 4]
 print(isMonotonic(A))
 print(isMonotonic(A))
 
+
+# Check if given array is Monotonic
+def isMonotonic(A):
+    x, y = [], []
+    x.extend(A)
+    y.extend(A)
+    x.sort()
+    y.sort(reverse=True)
+    if(x == A or y == A):
+        return True
+    return False
+ 
+ 
+# Driver program
+A = [6, 5, 4, 4]
+ 
+# Print required result
+print(isMonotonic(A))
+print(isMonotonic(A))
+
